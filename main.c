90b162b46233a0a5c68c05afd3faa27f22450e44
Emi <samafyan062@gmail.com>
2024-10-01 00:27:52 +0330

    complete random password generator

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <windows.h>
+#include <stdlib.h> // for srand
+#include <time.h> // for time
 
 void bar(char* message , int counter)
 {
@@ -11,9 +13,7 @@ void bar(char* message , int counter)
         printf("%c" , message[i]);
         Sleep(30); // 30 millisecond
     }
-    printf("\n\n");
 }
-
 
 void menu()
 {
@@ -26,12 +26,80 @@ void menu()
     int tcount = sizeof(tmsg) / sizeof(tmsg[0]);
 
     bar(fmsg , fcount);
+    printf("\n\n");
     bar(smsg , scount);
+    printf("\n\n");
     bar(tmsg , tcount);
+    printf("\n\n");
+    
 }   
+
+
+void password(int password_length , int choice , int counter , FILE *file)
+{
+    char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm,.<>/?;:'|{}[]!@#$^&*()_+=/*\"";
+
+    if(choice == 1)
+    {
+        printf("\n");
+
+        printf("\t password -> ");
+
+        for(int i = 0; i < password_length; i++)
+        {
+            printf("%c" , list[rand() % (sizeof (list) - 1)]);
+        }   
+        printf("\n");
+    }
+    else if(choice == 2)
+    {
+        for(int j = 0; j < counter; j++)
+        {
+            for(int i = 0; i < password_length; i++)
+                {
+                    fprintf(file, "%c", list[rand() % (sizeof(list) - 1)]);
+                }
+            fprintf(file , "\n");
+        }
+    }
+}
+
 
 int main()
 {
-    menu(); 
+    FILE *file = fopen("password.txt" , "w");
+
+    char fmsg[] = "Enter a number -> ";
+    char smsg[] = "Enter password length -> ";
+    char tmsg[] = "How many password you want -> ";
+
+    int password_length , counter , choice;
+    int fcount = sizeof(fmsg) / sizeof(fmsg[0]);
+    int scount = sizeof(smsg) / sizeof(smsg[0]);
+    int tcount = sizeof(tmsg) / sizeof(tmsg[0]);
+
+    menu();
+    bar(fmsg , fcount);
+    scanf("%d" , &choice);
+
+    if(choice == 1)
+    {
+        bar(smsg , scount);
+        scanf("%d" , &password_length);
+    }
+    else if (choice == 2)
+    {   
+        bar(smsg , scount);
+        scanf("%d" , &password_length);
+        bar(tmsg , tcount);
+        scanf("%d" , &counter);
+    }
+
+    srand(time(NULL));
+
+    password(password_length , choice , counter , file);
+
+    fclose(file);
+
     return 0;
 }
