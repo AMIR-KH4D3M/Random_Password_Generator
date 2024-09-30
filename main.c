1716d6448561528ccabcfd8dd676460e786a7b27
Emi <samafyan062@gmail.com>
2024-09-30 23:49:45 +0330

    create a simple menu

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
+// project name -> password generator
+// programmer -> Amir Khadem
+
 #include <stdio.h>
+#include <windows.h>
+
+void bar(char* message , int counter)
+{
+    for(int i = 0;i < counter;i++)
+    {
+        printf("%c" , message[i]);
+        Sleep(30); // 30 millisecond
+    }
+    printf("\n\n");
+}
+
+
+void menu()
+{
+    char fmsg[] = "Welcome to the password generator";
+    char smsg[] = "[1] >>> create a random password";
+    char tmsg[] = "[2] >>> create a random password list";
+
+    int fcount = sizeof(fmsg) / sizeof(fmsg[0]);
+    int scount = sizeof(smsg) / sizeof(smsg[0]);
+    int tcount = sizeof(tmsg) / sizeof(tmsg[0]);
+
+    bar(fmsg , fcount);
+    bar(smsg , scount);
+    bar(tmsg , tcount);
+}   
 
 int main()
 {
-    
+    menu(); 
     return 0;
 }
